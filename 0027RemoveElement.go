dc3ab1448f69235c6a547b1e8090870b5cac51a3
xavier <xieyuehao@gmail.com>
2020-01-01 14:54:17 +1000

    remove element

---
+++ 0027RemoveElement.go
@@ -0,0 +1,14 @@
+func removeElement(nums []int, val int) int {
+    //corner case
+    if nums == nil || len(nums) == 0{
+        return 0
+    }
+    res := 0 
+    for i := 0; i< len(nums) ; i++{
+        if nums[i] != val{
+            nums[res] = nums[i]
+            res += 1
+        }
+    }
+    return res
+}
