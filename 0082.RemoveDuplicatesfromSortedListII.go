b00b20d05d8ca08780c54b2e8d1d4219f95f60fd
xiexav01 <xavier.xie@csgi.com>
2020-01-08 14:20:35 +1000

    add 82. Remove Duplicates from Sorted List II

---
+++ 0082.RemoveDuplicatesfromSortedListII.go
@@ -0,0 +1,25 @@
+package main
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	pre := &ListNode{-1, nil}
+	cur := pre
+	pre.Next = head
+
+	for head != nil && head.Next != nil {
+		if head.Val != head.Next.Val {
+			head = head.Next
+			cur = cur.Next
+		} else {
+			for head != nil && head.Next != nil && head.Next.Val == head.Val {
+				head = head.Next
+			}
+			if head == nil {
+				cur.Next = nil
+			} else {
+				cur.Next = head.Next
+				head = head.Next
+			}
+		}
+	}
+	return pre.Next
+}
--- main.go
+++ main.go
@@ -12,7 +12,28 @@ func main() {
 	// print(res)
 	// A := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	// longestMountain(A)
-	A := []int{1,1,2,2,2,2}
-	target := 5
-	threeSumMulti(A, target)
+	// A := []int{1,1,2,2,2,2}
+	// target := 5
+	// threeSumMulti(A, target)
+	arr := []int{1, 2, 3, 3, 4, 4, 5}
+	head := convertArrayToHead(arr)
+	deleteDuplicates2(head)
+}
+
+func convertArrayToHead(arr []int) *ListNode {
+	pre := &ListNode{0, nil}
+	head := &ListNode{0, nil}
+	pre.Next = head
+	cur := head
+	for i := 0; i < len(arr); i++ {
+		cur.Val = arr[i]
+		cur.Next = &ListNode{0, nil}
+		if i < len(arr)-1 {
+			cur = cur.Next
+		} else {
+			cur.Next = nil
+		}
+	}
+
+	return pre.Next
 }
