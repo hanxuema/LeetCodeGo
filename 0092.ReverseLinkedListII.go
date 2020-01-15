93caab465bf633e01d8d972042c59ae839df133c
Xavier Xie <xiexav01@CSGICORP.COM>
2020-01-15 13:37:03 +1000

    git push

--- 0092.ReverseLinkedListII.go
+++ 0092.ReverseLinkedListII.go
@@ -32,3 +32,42 @@ package main
     c2.Next = cur
     return head
 }
+
+// my implementation
+// func reverseBetween(head *ListNode, m int, n int) *ListNode {
+//     if head == nil || m == n {
+//         return head
+//     }
+    
+//     length := n - m
+//     var tail *ListNode
+//     if m > 1 {
+//         tail = &ListNode{-1,head}
+//     }
+//     cur := head
+
+//     for m > 1 {
+//         tail = tail.Next
+//         cur = cur.Next
+//         m--
+//     }
+//     newCur := cur
+//     var pre *ListNode
+//     next := cur.Next
+//     for length >= 0 {
+//         next = cur.Next
+//         cur.Next = pre
+//         pre = cur
+//         cur = next
+//         length--
+//     }
+//     // next := cur.Next
+//     if tail != nil {
+//         tail.Next = pre
+//         newCur.Next = next
+//     } else {
+//         head = pre
+//         newCur.Next = next
+//     }
+//     return head
+// }
