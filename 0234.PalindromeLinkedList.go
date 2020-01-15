6fd5a8e9ca8461e0fc1839d9e0c96acdde612532
Xavier Xie <xiexav01@CSGICORP.COM>
2020-01-15 14:30:30 +1000

    234. Palindrome Linked List

--- 0234.PalindromeLinkedList.go
+++ 0234.PalindromeLinkedList.go
@@ -1,39 +1,36 @@
 package main
 
 func isPalindrome(head *ListNode) bool {
-    if head == nil {
-        return true
-    }
-    var pre *ListNode
-    slow, fast := head, head
-    for fast.Next != nil && fast.Next.Next != nil {
-        next := slow.Next
-        slow.Next = pre
-        pre = slow
-        slow = next
-        
-        fast = fast.Next.Next
-    }
-    var first *ListNode
-    var second *ListNode
-    if fast != nil { 
-        //   p  s     f   
-        //1<-2  3->2->1
-        first = pre
-        second = slow.Next
-    } else {
-        first = pre
-        second = slow
-    }  
-    
-    for second != nil{
-        if first.Val != second.Val {
-            return false
-        }
-        first = first.Next
-        second = second.Next
-    }
-     
-    return true
-    
-} 
+	if head == nil {
+		return true
+	}
+	//find middle
+	var pre *ListNode
+	slow, fast := head, head
+	for fast.Next != nil && fast.Next.Next != nil {
+		fast = fast.Next.Next
+
+		next := slow.Next
+		slow.Next = pre
+		pre = slow
+		slow = next
+	}
+	var left *ListNode
+	var right *ListNode
+	if fast.Next == nil {
+		left = pre
+		right = slow.Next
+	} else { //if fast.Next.Next == nil
+		right = slow.Next
+		slow.Next = pre
+		left = slow
+	}
+	for left != nil && right != nil {
+		if left.Val != right.Val {
+			return false
+		}
+		left = left.Next
+		right = right.Next
+	}
+	return true
+}
