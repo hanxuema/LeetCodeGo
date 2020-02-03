b6224b49058ef7bd3525dc396be4253105721d1f
xavier <xieyuehao@gmail.com>
2020-02-03 21:12:38 +1000

    0033.SearchinRotatedSortedArray.go

--- 0033.SearchinRotatedSortedArray.go
+++ 0033.SearchinRotatedSortedArray.go
@@ -6,30 +6,25 @@ func search(nums []int, target int) int {
         return -1
     }
     
-    left, right  := 0, len(nums) - 1
-    
-    for left <= right {
-		mid := left + (right - left) / 2
-        if nums[mid] > nums[right]{
-            left = mid - 1
-        } else {
-            right = mid - 1
-        }
-    }
-    right = len(nums) - 1
-    if nums[left] == nums[0] {
-        left = 0
-    } 
-    
+    left, right := 0, len(nums) - 1
     for left <= right {
         mid := left + (right - left) / 2
-        if nums[mid] == target{
-          return mid  
-        }
-        if nums[mid] > target{ 
-            right = mid - 1
-        } else {
-            left = mid + 1
+        if nums[mid] == target {
+            return mid
+        } else if target >= nums[0]{
+            if nums[mid] < target && nums[0] <= nums[mid]{
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        } else if target < nums[0]{
+            if nums[mid] < target {
+                left = mid + 1
+            } else if nums[mid] >= nums[0] {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
         }
     }
     return -1
