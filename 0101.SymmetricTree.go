45daaf773e6c5fa55f6c8c92c7591b9736118dec
xavier <xieyuehao@gmail.com>
2020-02-08 20:23:40 +1000

    0101 isSymmetricIterative

---
+++ 0101.SymmetricTree.go
@@ -0,0 +1,54 @@
+package main
+
+func isSymmetricIterative(root *TreeNode) bool {
+    if root == nil {
+        return true
+    }
+    if root.Left == nil && root.Right == nil {
+        return true
+    }
+    if root.Left == nil || root.Right == nil{
+        return false
+    }
+    if root.Left.Val != root.Right.Val {
+        return false
+    }
+    var q []*TreeNode
+    q = append(q, root.Left)
+    q = append(q, root.Right)
+    for len(q) > 0 {
+        //add next level to array
+        length := len(q)
+        level := []int{}
+        for i := 0; i < length; i++{
+            node := q[0]
+            var nodeVal int
+            if node != nil{
+                nodeVal = node.Val
+                q = append(q, node.Left)
+                q = append(q, node.Right)
+            }
+            level = append(level, nodeVal)
+            q = q[1:]
+        }
+        left, right := 0, len(level) - 1
+        for left < right {
+            if level[left] != level[right] {
+                return false
+            }
+            left++
+            right--
+        }
+    }
+    return true
+}
+
+
+// [1,2,2,3,4,4,3]
+// [1,2,2,3,4,4,3]
+// [1,2,2,null,3,null,3]
+// [1,2,2,null,3,3,null]
+// [1]
+// [1,2,2,3,4,4,3,5,6,7,8,8,7,6,5]
+// [1,2,2,3,4,4,3,5,6,7,8,8,7,7,5]
+// [1,2,2,3,4,4,3,5,6,7,8,8,7,6,5,9]
