11004be84d4b628a5197afcd34ff165ab348bcfd
xavier <xieyuehao@gmail.com>
2020-02-08 16:08:19 +1000

    0104.MaximumDepthofBinaryTree.go

---
+++ 0104.MaximumDepthofBinaryTree.go
@@ -0,0 +1,21 @@
+package main
+
+func maxDepth(root *TreeNode) int {
+    depth := 0
+    return findMax(root, depth)
+}
+
+func findMax(root *TreeNode, depth int) int{
+    if root == nil {
+        return depth
+    } 
+    return max(findMax(root.Left, depth + 1),findMax(root.Right, depth + 1))
+}
+
+// func max(a,b int)int{
+//     if a > b{
+//         return a
+//     } else {
+//         return b
+//     }
+// }
