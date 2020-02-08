cbe1f4636ccdaccafd62e5b36afc3616fc4a3b4a
xavier <xieyuehao@gmail.com>
2020-02-08 15:33:04 +1000

    144. Binary Tree Preorder Traversal

--- 0034.FindFirstandLastPositionofElementinSortedArray.go
+++ 0034.FindFirstandLastPositionofElementinSortedArray.go
@@ -1,33 +1,36 @@
 package main
 
 func searchRange(nums []int, target int) []int {
-	res := []int{-1, -1}
-	if nums == nil || len(nums) == 0 {
-		return res
-	}
-	left, right := 0, len(nums)
-	for left < right {
-		mid := left + (right-left)/2
-		if nums[mid] >= target {
-			right = mid
-		} else if nums[mid] < target {
-			left = mid + 1
-		}
-	}
-	if left >= len(nums) || nums[left] > target {
-		return res
-	}
-	res[0] = left
-	left, right = 0, len(nums)
-	for left < right {
-		mid := left + (right-left)/2
-		if nums[mid] <= target {
-			left = mid + 1
-		} else if nums[mid] > target {
-			right = mid
-		}
-	}
-	res[1] = right - 1
-
-	return res
+    res := []int{-1,-1}
+    if nums == nil || len(nums) == 0 {
+        return res
+    }
+    //find the left
+    left, right := 0, len(nums)
+    for left < right {
+        mid := left + (right - left) / 2
+        if nums[mid] >= target {
+            right = mid
+        } else if nums[mid] < target {
+            left = mid + 1
+        }
+    }
+    if left > len(nums) - 1 || nums[left] > target {
+        return res
+    }
+    
+    res[0] = left
+    //find the right
+    
+    left, right = 0, len(nums) 
+    for left < right {
+        mid := left + (right - left) / 2
+        if nums[mid] <= target {
+            left = mid + 1
+        } else if nums[mid] > target {
+            right = mid
+        }
+    }
+    res[1] = right - 1
+    return res
 }
---
+++ 0144.BinaryTreePreorderTraversal.go
@@ -0,0 +1,20 @@
+package main
+
+ 
+  type TreeNode struct {
+      Val int
+      Left *TreeNode
+      Right *TreeNode
+  }
+ 
+ func preorderTraversal(root *TreeNode) []int {
+    if root == nil{
+        return nil
+    }
+    res := []int{}
+    res = append(res, root.Val)
+    res = append(res, preorderTraversal(root.Left)...)
+    res = append(res, preorderTraversal(root.Right)...)
+    
+    return res
+}
