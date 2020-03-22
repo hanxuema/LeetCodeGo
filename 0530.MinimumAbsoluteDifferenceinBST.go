b466c995c7c6563d669a02666c4d828cfa96a4a4
xavier <xieyuehao@gmail.com>
2020-03-22 14:40:17 +1000

    530. Minimum Absolute Difference in BST

---
+++ 0530.MinimumAbsoluteDifferenceinBST.go
@@ -0,0 +1,27 @@
+package main
+
+func getMinimumDifference(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    arr := []int{}
+    inorder530(root, &arr)
+  
+    res :=  math.MaxInt32
+ 
+    for i:= 1; i < len(arr); i++{
+        if arr[i] - arr[i-1] < res{
+            res = arr[i] - arr[i-1]
+        }
+    }
+    return res
+}
+
+func inorder530(root *TreeNode, arr *[]int){
+    if root == nil{
+        return
+    }
+    inorder530(root.Left, arr)
+    *arr = append(*arr, root.Val)
+    inorder530(root.Right, arr)
+}
