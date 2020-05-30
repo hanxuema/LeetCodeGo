7c090f893cceaa12bab8d2979604c0704d0ef423
xavier <xieyuehao@gmail.com>
2020-05-30 22:39:13 +1000

    0009 palindromeNumber

---
+++ 0009.PalindromeNumber.go
@@ -0,0 +1,22 @@
+package main
+
+func isPalindrome(x int) bool {
+    if x < 0 {
+        return false
+    }
+    if x == 0 {
+        return true
+    }
+    origin := x
+    var num int
+    for x > 0 {
+        temp := x % 10
+        x = x / 10
+        num = num * 10 + temp
+        if num < -2147483648 || num > 2147483647{
+            return false
+        }
+    }
+    fmt.Println(num)
+    return num == origin
+}
